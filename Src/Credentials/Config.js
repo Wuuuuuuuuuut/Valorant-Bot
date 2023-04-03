a589cec8f604c56d027a39cc81ff3cc43c75c74d
Wuuuuuuuuuut <71558901+Wuuuuuuuuuut@users.noreply.github.com>
2023-04-03 23:53:00 +0200

    REMOVED MY TOKEN

--- Src/Credentials/Config.js
+++ Src/Credentials/Config.js
@@ -1,5 +1,5 @@
 module.exports = {
     prefix: ["!"],
     developersIds: ["422042586610663425"],
-    botToken: 'MTA4NDE1Mjc1MTU4NTUwOTM5Nw.G0mOmV.5urren-scrZBKLdcibTSQPZmVjYWevtT1SN9a8',
+    botToken: 'DISCORD_TOKEN_HERE',
 }
